4df20ee3085b69d95061b4c7e3c130532d57c202
dorianprogrammer <drodriguez@csye.com>
2023-05-06 19:01:01 -0600

    cambios

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://ticketing-app.shop",
       headers: req.headers,
     });
   } else {
